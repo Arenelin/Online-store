62ba8e87610bbe3ecb94cf7c09ce8d563502c3f1
Nikita <belov.1098@mail.ru>
2023-08-18 14:41:56 +0700

    refactoring code for optimization

--- js/slider.js
+++ js/slider.js
@@ -1,66 +1,27 @@
 'use strict'
 
-const arrayOfBtnsTabs = document.querySelectorAll('.tabs > button');
 
-
-
+//Создание первоначального вида страницы при загрузке
 let tabActiveId = "goods";
-let activeTab = search();
+let activeTab = findActiveBtnTab();
 activeTab.classList.add('active');
 
-const tabs = document.querySelector('.tabs');
-tabs.insertAdjacentHTML("afterend", renderGoods());
+//Нахождение и присвоение обработчика для табов
+const arrayOfBtnsTabs = document.querySelectorAll('.tabs > button');
+addListenerForBtns(arrayOfBtnsTabs, clickHandler);
 
-
-
-
-
-const arrayOfBtnsCart = document.querySelectorAll('button[data-count="true"]');
-
-for (let i = 0; i < arrayOfBtnsCart.length; i++){
-   arrayOfBtnsCart[i].addEventListener('click', arrayCount);
-}
-
-let array = [];
-const tabWithCounter = document.querySelector('button[data-goods-count]');
-console.log(tabWithCounter);
-function arrayCount() {
-   let product = productItem();
-   array.push(product);
-   tabWithCounter.dataset.goodsCount = array.length;
-}
-function productItem() {
-   return {
-      name: "Серая футболка",
-      price: 1800,
-   }
-}
-
-
-
-for (let i = 0; i < arrayOfBtnsTabs.length; i++) {
-   arrayOfBtnsTabs[i].addEventListener('click', clickHandler);
-}
-
-
-
-
+//Создание табов
 function clickHandler(event) {
-   activeTab = search();
+   activeTab = findActiveBtnTab();
    activeTab.classList.remove('active');
    event.target.classList.add('active');
    tabActiveId = event.target.dataset.id;
-   remove();
-   create(tabActiveId);
+   removeNotActiveTab();
+   createActiveTab(tabActiveId);
 }
 
-function search() {
-   return document.querySelector(`button[data-id="${tabActiveId}"]`);
-}
-
-
-
-function create(tabId) {
+//Заполнение HTML при переключении табов
+function createActiveTab(tabId) {
    let html = '';
    if (tabId === "goods") {
       html = renderGoods();
@@ -70,31 +31,49 @@ function create(tabId) {
    tabs.insertAdjacentHTML("afterend", html)
 }
 
-
-function remove() {
+//Удаление переключенного таба
+function removeNotActiveTab() {
    const activeTabContent = document.querySelector('[data-active-tab-content="true"]');
    activeTabContent.remove();
 }
 
+const tabs = document.querySelector('.tabs');
+tabs.insertAdjacentHTML("afterend", renderGoods());
 
+function findActiveBtnTab() {
+   return document.querySelector(`button[data-id="${tabActiveId}"]`);
+}
 
+//Нахождение и присвоение обработчика для кнопок "В корзину"
+const arrayOfBtnsCart = document.querySelectorAll('button[data-count="true"]');
+addListenerForBtns(arrayOfBtnsCart, addProductsInCart);
 
+//Цикл для добавления слушателя к кнопкам
+function addListenerForBtns(elements, callback) {
+   for (let i = 0; i < elements.length; i++){
+      elements[i].addEventListener('click', callback);
+   }
+}
 
+//Создание массива для заполнения корзины
+const arrayOfProductsInCart = [];
+const tabWithCounter = document.querySelector('button[data-goods-count]');
+function addProductsInCart() {
+   let product = createProductItem();
+   arrayOfProductsInCart.push(product);
+   tabWithCounter.dataset.goodsCount = arrayOfProductsInCart.length;
+}
+function createProductItem() {
+   return {
+      name: "Серая футболка",
+      price: 1800,
+   }
+}
 
-
-
-
-
-
-
-
-
-
-
-
+//Контент товаров
 function renderGoods() {
    return `
-   <div data-active-tab-content="true" class="product-items item">
+   <div data-active-tab-content="true" class="product-items">
    <div class="item">
      <img src="/img/01.png" class= "item__img" alt ="t-shirt one">
      <div class="item__descr">
@@ -123,6 +102,7 @@ function renderGoods() {
    `;
 }
 
+//Контент корзины
 function renderCart() {
    return `
    <div data-active-tab-content="true" class="cart-items cart">
